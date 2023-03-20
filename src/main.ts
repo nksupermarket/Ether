b886e427a7ee5fcb3a9c39966ac1b1fbc7e36081
alex <alexlhliang@gmail.com>
2023-03-20 00:45:01 -0700

    removed Image module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { runClock } from "./Date";
-import initImage from "./Image";
 import initLinks from "./collectionLinks";
 import initModal from "./Modal";
 import "./styles/style.css";
 
 async function init() {
-  await initImage();
   runClock();
   initLinks();
   initModal();
