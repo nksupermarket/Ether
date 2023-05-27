52989112727ce046d0c572d2d9eaf222d0af5f9f
alex <alexlhliang@gmail.com>
2023-05-27 13:23:39 -0700

    LocalStorage.getAll only pulls from specificied keys now instead of the entire localStorage

--- src/LocalStorage.ts
+++ src/LocalStorage.ts
@@ -1,9 +1,18 @@
+import { getImage, IMAGE_LS_KEY } from "./Image";
+import { getKeyBinds, KEYBINDS_LS_KEY } from "./KeyBinds";
+import { getLinks, LINKS_LS_KEY } from "./Links";
+import { getSearch, SEARCH_LS_KEY } from "./Search";
+import { getTheme, THEME_LS_KEY } from "./Theme";
+
 const LocalStorage = {
   getAll: function () {
-    const config = JSON.parse(JSON.stringify(localStorage));
-    for (const key of Object.keys(config)) {
-      config[key] = JSON.parse(config[key]);
-    }
+    const config = {
+      [THEME_LS_KEY]: getTheme(),
+      [LINKS_LS_KEY]: getLinks(),
+      [KEYBINDS_LS_KEY]: getKeyBinds(),
+      [IMAGE_LS_KEY]: getImage(),
+      [SEARCH_LS_KEY]: getSearch(),
+    };
     return config;
   },
   format: function () {
